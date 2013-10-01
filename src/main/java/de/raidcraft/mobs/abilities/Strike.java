85eb4f39ef4bbed8353d988b5d1547babd8d30c8
Silthus <silthus@raid-craft.de>
2013-10-01 17:30:13 +0200

    made the meele strike attack trigger on meele attacks

--- src/main/java/de/raidcraft/mobs/abilities/Strike.java
+++ src/main/java/de/raidcraft/mobs/abilities/Strike.java
@@ -3,13 +3,14 @@ package de.raidcraft.mobs.abilities;
 import de.raidcraft.mobs.api.Mob;
 import de.raidcraft.mobs.api.MobAbility;
 import de.raidcraft.skills.api.ability.AbilityInformation;
-import de.raidcraft.skills.api.ability.Useable;
+import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.EffectType;
-import de.raidcraft.skills.api.combat.action.EntityAttack;
-import de.raidcraft.skills.api.combat.callback.EntityAttackCallback;
+import de.raidcraft.skills.api.combat.action.AbilityAction;
+import de.raidcraft.skills.api.combat.action.Attack;
 import de.raidcraft.skills.api.effect.common.SunderingArmor;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.AbilityProperties;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
 import de.raidcraft.skills.effects.Bleed;
 import de.raidcraft.skills.effects.Burn;
 import de.raidcraft.skills.effects.Slow;
@@ -19,6 +20,7 @@ import de.raidcraft.skills.effects.disabling.Interrupt;
 import de.raidcraft.skills.effects.disabling.KnockBack;
 import de.raidcraft.skills.effects.disabling.Silence;
 import de.raidcraft.skills.effects.disabling.Stun;
+import de.raidcraft.skills.trigger.AttackTrigger;
 import org.bukkit.configuration.ConfigurationSection;
 
 /**
@@ -29,7 +31,7 @@ import org.bukkit.configuration.ConfigurationSection;
         description = "A default strike with many custom options.",
         types = {EffectType.PHYSICAL, EffectType.DAMAGING}
 )
-public class Strike extends MobAbility implements Useable {
+public class Strike extends MobAbility {
 
     private boolean knockBack = false;
     private boolean bleed = false;
@@ -64,25 +66,28 @@ public class Strike extends MobAbility implements Useable {
         silence = data.getBoolean("silence", false);
     }
 
-    @Override
-    public void use() throws CombatException {
+    @TriggerHandler(ignoreCancelled = true)
+    public void onAttack(AttackTrigger trigger) throws CombatException {
 
-        attack(getTarget(), new EntityAttackCallback() {
-            @Override
-            public void run(EntityAttack attack) throws CombatException {
+        if (!trigger.getAttack().isOfAttackType(EffectType.PHYSICAL)) {
+            return;
+        }
+        Attack<?,CharacterTemplate> attack = trigger.getAttack();
 
-                if (knockBack) addEffect(getHolder().getEntity().getLocation(), attack.getTarget(), KnockBack.class);
-                if (bleed) addEffect(attack.getTarget(), Bleed.class);
-                if (stun) addEffect(attack.getTarget(), Stun.class);
-                if (sunderArmor) addEffect(attack.getTarget(), SunderingArmor.class);
-                if (disarm) addEffect(attack.getTarget(), Disarm.class);
-                if (ignoreArmor)attack.addAttackTypes(EffectType.IGNORE_ARMOR);
-                if (slow) addEffect(attack.getTarget(), Slow.class);
-                if (weaken)addEffect(attack.getTarget(), Weakness.class);
-                if (burn) addEffect(attack.getTarget(), Burn.class);
-                if (silence || interrupt) addEffect(attack.getTarget(), Interrupt.class);
-                if (silence) addEffect(attack.getTarget(), Silence.class);
-            }
-        });
+        if (knockBack) addEffect(getHolder().getEntity().getLocation(), attack.getTarget(), KnockBack.class);
+        if (bleed) addEffect(attack.getTarget(), Bleed.class);
+        if (stun) addEffect(attack.getTarget(), Stun.class);
+        if (sunderArmor) addEffect(attack.getTarget(), SunderingArmor.class);
+        if (disarm) addEffect(attack.getTarget(), Disarm.class);
+        if (ignoreArmor) attack.addAttackTypes(EffectType.IGNORE_ARMOR);
+        if (slow) addEffect(attack.getTarget(), Slow.class);
+        if (weaken) addEffect(attack.getTarget(), Weakness.class);
+        if (burn) addEffect(attack.getTarget(), Burn.class);
+        if (silence || interrupt) addEffect(attack.getTarget(), Interrupt.class);
+        if (silence) addEffect(attack.getTarget(), Silence.class);
+
+        attack.setDamage(getTotalDamage());
+
+        substractUsageCost(new AbilityAction<>(this));
     }
 }
