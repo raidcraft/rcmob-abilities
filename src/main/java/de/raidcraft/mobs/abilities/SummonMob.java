e74368d78e72dd34335ff4cccee3449cff81bca0
Silthus <silthus@raid-craft.de>
2013-10-05 23:10:32 +0200

    kill spawned mobs when leaving combat

--- src/main/java/de/raidcraft/mobs/abilities/SummonMob.java
+++ src/main/java/de/raidcraft/mobs/abilities/SummonMob.java
@@ -8,10 +8,19 @@ import de.raidcraft.mobs.api.Mob;
 import de.raidcraft.mobs.api.MobAbility;
 import de.raidcraft.skills.api.ability.AbilityInformation;
 import de.raidcraft.skills.api.ability.Useable;
+import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.events.RCCombatEvent;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.AbilityProperties;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.CombatTrigger;
+import de.raidcraft.skills.trigger.EntityDeathTrigger;
 import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Silthus
@@ -21,9 +30,12 @@ import org.bukkit.configuration.ConfigurationSection;
         description = "Summons mobs into the casters party.",
         types = {EffectType.HEALING, EffectType.SUMMON}
 )
-public class SummonMob extends MobAbility implements Useable {
+public class SummonMob extends MobAbility implements Useable, Triggered {
 
+    private final List<CharacterTemplate> spawnedMobs = new ArrayList<>();
     private SpawnableMob spawnableMob;
+    private boolean killOnDeath;
+    private boolean killOnCombatLeave;
     private int amount;
 
     public SummonMob(Mob holder, AbilityProperties data) {
@@ -37,8 +49,34 @@ public class SummonMob extends MobAbility implements Useable {
         try {
             spawnableMob = RaidCraft.getComponent(MobManager.class).getSpwanableMob(data.getString("mob"));
             amount = data.getInt("amount", 1);
+            killOnDeath = data.getBoolean("kill-on-death", false);
+            killOnCombatLeave = data.getBoolean("kill-on-combat-leave", true);
         } catch (UnknownMobException e) {
             RaidCraft.LOGGER.warning(e.getMessage());
+        }
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onDeath(EntityDeathTrigger trigger) {
+
+        if (killOnDeath) {
+            for (CharacterTemplate characterTemplate : spawnedMobs) {
+                characterTemplate.kill(getHolder());
+            }
+        }
+        spawnedMobs.clear();
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onCombatLeave(CombatTrigger trigger) {
+
+        if (trigger.getEvent().getType() == RCCombatEvent.Type.LEAVE) {
+            if (killOnCombatLeave) {
+                for (CharacterTemplate characterTemplate : spawnedMobs) {
+                    characterTemplate.kill(getHolder());
+                }
+            }
+            spawnedMobs.clear();
         }
     }
 
@@ -49,7 +87,7 @@ public class SummonMob extends MobAbility implements Useable {
             return;
         }
         for (int i = 0; i < amount; i++) {
-            spawnableMob.spawn(getHolder().getEntity().getLocation());
+            spawnedMobs.addAll(spawnableMob.spawn(getHolder().getEntity().getLocation()));
         }
     }
 }
